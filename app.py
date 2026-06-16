84c7ae403017dc208e2796409bfc32bbb806f7d0
jhoana-gonzales <jhoanagonzalescaballero@gmail.com>
2026-06-15 21:02:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # ==============================================================================
 # LIBRERÍAS REQUERIDAS
 # ==============================================================================
-import streamlit as tf
+import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
